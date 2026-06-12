5f4debb3b8e6c72c137c3b1c7c63fd3d4bf7bbe4
Minouf452 <pablo.nalbanti@gmail.com>
2026-06-12 11:06:18 +0200

    Initial commit

--- server.py
+++ server.py
@@ -4,14 +4,27 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/especes")
+def especes():
+
+    return send_file(
+        "species_taxonomy_with_ott.csv"
+    )
+
 @app.route("/")
 def accueil():
     return render_template("Phylo.html")
+
+from flask import send_file
 
 @app.route("/prune", methods=["POST"])
 def prune():
 
     tax_ids = request.json.get("tax_ids", [])
+
+    base_dir = os.path.dirname(
+        os.path.abspath(__file__)
+    )
 
     cmd = [
         "py", "-3.11",
@@ -21,9 +34,17 @@ def prune():
         "-t"
     ] + tax_ids
 
-    subprocess.run(cmd, capture_output=True, text=True)
+    subprocess.run(
+        cmd,
+        capture_output=True,
+        text=True,
+        cwd=base_dir
+    )
 
     return {"image": "/static/tree.png"}
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(
+        port=5000,
+        debug=True
+    )
